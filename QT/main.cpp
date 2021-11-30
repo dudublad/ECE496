99e40c8c92c08b4a08e65cd8f35d7a5db909f2dd
Eduardo Stecca Ortenblad <edu.ortenblad@mail.utoronto.ca>
2021-11-30 16:54:29 -0500

    clean up main.cpp

--- QT/main.cpp
+++ QT/main.cpp
@@ -1,13 +1,8 @@
 #include "mainwindow.h"
 #include "TestClass.h"
-#include "SineWave.h"
-#include "Noise.h"
 #include <iostream>
 
 #include <QApplication>
-
-//Link Ws2_32.lib for STK
-#pragma comment(lib, "Ws2_32.lib")
 
 int main(int argc, char *argv[])
 {
