b9b8482e81097a204b23fe371f1322c8b32c7d6a
paytin.gardner@gmail.com <paytin.gardner@gmail.com>
2022-03-09 07:50:45 -0500

    fixed FFT magnitude scaling, improved FFTW speed

--- backend/fft.cpp
+++ backend/fft.cpp
@@ -9,10 +9,12 @@
 QVector<double> fft(QVector<double> in_wave){
     int in_size = in_wave.size();
     int out_size = floor(in_size/2)+1;
+    QVector<double> out_wave(out_size);
+
 
     double* mFftIn  = fftw_alloc_real(in_size);
     fftw_complex* mFftOut = fftw_alloc_complex(out_size);
-    fftw_plan mFftPlan = fftw_plan_dft_r2c_1d(in_size, mFftIn, mFftOut, FFTW_PRESERVE_INPUT);
+    fftw_plan mFftPlan = fftw_plan_dft_r2c_1d(in_size, mFftIn, mFftOut, FFTW_ESTIMATE);
 
     //in_wave->mFftIn
     std::copy(in_wave.begin(), in_wave.end(), mFftIn);
@@ -21,9 +23,13 @@ QVector<double> fft(QVector<double> in_wave){
     fftw_execute(mFftPlan);
 
     //Convert cplx output to magnitude, and copy into output vector
-    QVector<double> out_wave(out_size);
     for(int i = 0; i < out_size; i++) {
         out_wave[i] = sqrt(pow(mFftOut[i][0],2) + pow(mFftOut[i][1],2));
+    }
+
+    double max_value = *std::max_element(out_wave.begin(), out_wave.end());
+    for(int i = 0; i < out_size; i++) {
+        out_wave[i]/=max_value;
     }
 
     //free allocated memory
