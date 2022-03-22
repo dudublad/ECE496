68993faec1daa5982f318b2b1cd00c4bb53369e4
paytin.gardner@gmail.com <paytin.gardner@gmail.com>
2022-03-22 18:56:59 -0400

    ipr: changing to enum

--- backend/generatefircoeff.h
+++ backend/generatefircoeff.h
@@ -11,6 +11,14 @@ typedef enum {
     BSF
 } FilterType;
 
+typedef enum {
+    rect,
+    hann,
+    hamm,
+    bart,
+    black
+} WindowType;
+
 //Coef generation function - call this one
 //output:
 //vector of floats - FIR filter coefficients (see stk::FIR)
@@ -21,30 +29,30 @@ typedef enum {
 //window_type should be one of: "rect", "hann", "hamm", "bart", "black"
 //window default should be "rect"
 //returns empty vector for error
-QVector<double> generateFIRCoeff(double fc1, double fc2, std::string filter_type, std::string window_type);
+QVector<double> generateFIRCoeff(double fc1, double fc2, FilterType filter_type, WindowType window_type);
 
 
 //IGNORE THESE --------------------------------------------
 //Specific generator functions
 //Do not call these!
-QVector<double> generate_fc(double ft1, double ft2, int M, std::string filter_type);
+QVector<double> generate_fc(double ft1, double ft2, int M, FilterType filter_type);
 QVector<double> generateLPF(double ft, int M);
 QVector<double> generateHPF(double ft, int M);
 QVector<double> generateBPF(double ft1, double ft2, int M);
 QVector<double> generateBSF(double ft1, double ft2, int M);
 
 //Window functions
-QVector<double> window_fc(QVector<double> input, int M, std::string window_type);
+QVector<double> window_fc(QVector<double> input, int M, WindowType window_type);
 QVector<double> window_hann(QVector<double> input, int M);
 QVector<double> window_hamm(QVector<double> input, int M);
 QVector<double> window_bart(QVector<double> input, int M);
 QVector<double> window_black(QVector<double> input, int M);
 
 //Used internally
-int MCheck(int M, std::string filter_type);
+int MCheck(int M, FilterType filter_type);
 double my_sinc(int i, int M, double f);
 
 //Used for testing - get rid of eventually probably
-void printCoeffs(QVector<double> input, std::string filter_type);
+void printCoeffs(QVector<double> input, FilterType filter_type);
 
 #endif // GENERATEFIRCOEFF_H
