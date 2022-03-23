5e859ed8e311f3317025fe57aaa17f15e3fe88ac
Maxarreaga <34686490+Maxarreaga@users.noreply.github.com>
2022-03-23 12:03:25 -0400

    added description

--- QT/tutorialpanel.h
+++ QT/tutorialpanel.h
@@ -10,6 +10,16 @@
  * This is the panel anchored to the top of the screen
  * that displays the current progress of this tutorial step
  */
+
+/*
+ * checkBox->setAttribute(Qt::WA_TransparentForMouseEvents, readOnly);
+   checkBox->setFocusPolicy(readOnly ? Qt::NoFocus : Qt::StrongFocus);
+ * TODO: for later
+ * You leave the checkbox as "enabled" and "checkable".
+ * This just makes it so the control
+ * ignores mouse events and can not have focus
+ *
+ */
 class TutorialPanel : public QWidget
 {
     Q_OBJECT
