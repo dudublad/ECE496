27a36c056dc1799c92b325bbe62bb990cf4214fa
paytin.gardner@gmail.com <paytin.gardner@gmail.com>
2022-03-26 07:40:50 -0400

    added fft_v which uses std::vector instead of QVector

--- backend/fft.cpp
+++ backend/fft.cpp
@@ -38,3 +38,36 @@ QVector<double> fft(QVector<double> in_wave){
 
     return out_wave;
 }
+
+std::vector<double> fft_v(std::vector<double> in_wave){
+    int in_size = in_wave.size();
+    int out_size = floor(in_size/2)+1;
+    std::vector<double> out_wave(out_size);
+
+
+    double* mFftIn  = fftw_alloc_real(in_size);
+    fftw_complex* mFftOut = fftw_alloc_complex(out_size);
+    fftw_plan mFftPlan = fftw_plan_dft_r2c_1d(in_size, mFftIn, mFftOut, FFTW_ESTIMATE);
+
+    //in_wave->mFftIn
+    std::copy(in_wave.begin(), in_wave.end(), mFftIn);
+
+    //execute FFT
+    fftw_execute(mFftPlan);
+
+    //Convert cplx output to magnitude, and copy into output vector
+    for(int i = 0; i < out_size; i++) {
+        out_wave[i] = sqrt(pow(mFftOut[i][0],2) + pow(mFftOut[i][1],2));
+    }
+
+    double max_value = *std::max_element(out_wave.begin(), out_wave.end());
+    for(int i = 0; i < out_size; i++) {
+        out_wave[i]/=max_value;
+    }
+
+    //free allocated memory
+    fftw_free(mFftIn);
+    fftw_free(mFftOut);
+
+    return out_wave;
+}
